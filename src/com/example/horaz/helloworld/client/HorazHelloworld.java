074c553f01095b7794bad458b3e86ddd446395c5
Jonas Gehring <jonas.gehring@inovex.de>
2012-03-29 14:42:36 +0200

    current horaz

--- src/com/example/horaz/helloworld/client/HorazHelloworld.java
+++ src/com/example/horaz/helloworld/client/HorazHelloworld.java
@@ -5,6 +5,9 @@ import com.google.gwt.user.client.Event;
 import com.horaz.client.widgets.Button;
 import com.horaz.client.widgets.Toast;
 import com.horaz.client.widgets.Toast.Duration;
+import com.horaz.client.widgets.charting.Plot;
+import com.horaz.client.widgets.charting.Point;
+import com.horaz.client.widgets.charting.Series;
 import com.horaz.client.widgets.events.TapListener;
 
 public class HorazHelloworld implements EntryPoint {
@@ -21,5 +24,11 @@ public class HorazHelloworld implements EntryPoint {
 				new Toast("I love Horaz!", Duration.SHORT).show();
 			}
 		});
+
+		// creates graph
+		Plot plot = Plot.byId("graph");
+		plot.setDimensions(300, 100);
+		plot.addSeries(new Series("Power", new Point(1, 2), new Point(2, 3)));
+		plot.render();
 	}
 }
